ece0ba44fe81aacd1f863e065d641b2288968775
master <tribbia@ucar.edu>
2022-07-04 15:06:42 -0600

    Compute wind chill factor

--- mysci.py
+++ mysci.py
@@ -1,6 +1,6 @@
 # Column names column indices
 columns={'date': 0, 'time' : 1,  'tempout' : 2, 'windspeed': 7}
-types={'tempout': float}
+types={'tempout': float, 'windspeed': float }
 #Initialize my data variable
 data= {'date':[],'time':[],'tempout':[]}
 for column in columns:
@@ -23,10 +23,10 @@ with open(filename,"r") as datafile:
             data[column].append(value)
 
 
-#def estimate_windchill(t,v):
-#    wci= t-0.7*v
-#    return wci
+def estimate_windchill(t,v):
+    wci= t-0.7*v
+    return wci
 
-#windchill=[]
-#for temp, windspeed in zip(data['tempout'], data['windspeed']):
-#    windchill.append(estimate_windchill(temp,windspeed))
+windchill=[]
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(estimate_windchill(temp,windspeed))
