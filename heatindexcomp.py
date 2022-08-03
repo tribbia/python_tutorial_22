8a9973b6207fca0b4eb5fbaeafaae139c102b846
master <tribbia@ucar.edu>
2022-08-03 15:05:27 -0600

    Copyimg first script to start second

---
+++ heatindexcomp.py
@@ -0,0 +1,38 @@
+#Column names column indices
+columns={'date': 0, 'time' : 1,  'tempout' : 2, 'windspeed': 7, 'windchill': 12}
+types={'tempout': float, 'windspeed': float , 'windchill' : float }
+#Initialize my data variable
+#data= {'date':[],'time':[],'tempout':[], 'windchill' :[]
+data ={} 
+for column in columns:
+    data[column]=[]
+# Read the data file
+filename="data/wxobs20170821.txt"
+with open(filename,"r") as datafile:
+    for _ in range(3):
+       	datafile.readline()
+    for line in datafile:
+        datum = line.split()
+        for column in columns:
+            i=columns[column]
+            t = types.get(column,str)
+            value =t(datum[i])
+            data[column].append(value)
+
+
+def estimate_windchill(t,v):
+    wci= t-0.7*v
+    return wci
+
+windchill=[]
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(estimate_windchill(temp,windspeed))
+# Output comparion of data
+print('                         ORIGINAL     ESTIMATED')
+print('  DATE        TIME       WINDCHILL    WNDCHILL     DIFFERENCE')
+print('  ______     ______      ________     ________     __________')
+
+zip_data = zip(data['date'],data['time'],data['windchill'], windchill)
+for date,time,wc_data, wc_est in zip_data:
+    wc_diff= wc_data - wc_est
+    print(f'  {date}   {time:>6}     {wc_data:9.6f}    {wc_est:9.6f}    {wc_diff:10.6f}') 
