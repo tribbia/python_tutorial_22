e821df51e1cddbc9b7b3f5a8f055b4449ce58412
master <tribbia@ucar.edu>
2022-08-04 12:56:51 -0600

    refactor scripts to use new module

--- windchillcomp.py
+++ windchillcomp.py
@@ -1,25 +1,11 @@
+
+from readdata import read_data
+
 #Column names column indices
 columns={'date': 0, 'time' : 1,  'tempout' : 2, 'windspeed': 7, 'windchill': 12}
 types={'tempout': float, 'windspeed': float , 'windchill' : float }
-#Initialize my data variable
-#data= {'date':[],'time':[],'tempout':[], 'windchill' :[]
-data ={} 
-for column in columns:
-    data[column]=[]
 # Read the data file
-filename="data/wxobs20170821.txt"
-with open(filename,"r") as datafile:
-    for _ in range(3):
-       	datafile.readline()
-    for line in datafile:
-        datum = line.split()
-        for column in columns:
-            i=columns[column]
-            t = types.get(column,str)
-            value =t(datum[i])
-            data[column].append(value)
-
-
+data= read_data(columns, types=types)
 def estimate_windchill(t,v):
     wci= t-0.7*v
     return wci
