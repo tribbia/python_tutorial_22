b175ae4327b0e9e4ad527103d19cfbcd2076d533
master <tribbia@ucar.edu>
2022-09-14 13:52:56 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "mysci",
+    version = "1.0.0",
+    description= " A sample package description",
+    author = "Joe Tribbia",
+    author_email = "tribbia@ucar.edu",
+    install_requires=[])    
